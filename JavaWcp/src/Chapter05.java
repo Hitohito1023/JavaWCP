c62fd4d6c6bda73c6c38f466cf74e378bdf1c5e9
kinaseryota <kinaseryota@192.168.1.13>
2021-02-08 12:14:19 +0900

    [finish]#5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		long long1 = 11111111111111111L;
+		double double1 = 0.222222222d;
+		boolean boolean1 = true;
+		
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float Float1 = 0.333333333f;
+		Integer Integer1 = 3;
+		Character Character1 = 'あ';
+		
+		System.out.println(Float1);
+		System.out.println(Integer1);
+		System.out.println(Character1);
+		
+		int[] array1 = new int[] {10, 30, 40, 50, 60 };
+		System.out.println(array1[3]);
+		
+	}
+}
