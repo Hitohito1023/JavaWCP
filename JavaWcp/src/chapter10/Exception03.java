bead6c6767a2a6a03ad8903f2de8d2f3b3ff90eb
kinaseryota <kinaseryota@192.168.1.3>
2021-03-16 16:05:20 +0900

    [finish]Chapter10

--- JavaWcp/src/chapter10/Exception03.java
+++ JavaWcp/src/chapter10/Exception03.java
@@ -3,7 +3,7 @@ package chapter10;
 public class Exception03 {
 	public static void main(String[] args) {
 		try {
-			Exception03.division(100, 0);
+//			Exception03.division(100, 0);
 		} catch (ArithmeticException e) {
 			System.out.println("ArithmeticException例外が発生");
 		} catch (IllegalArgumentException e) {
@@ -12,14 +12,14 @@ public class Exception03 {
 		}
 		System.out.println("プログラム終了");
 
-		public static void division(int a, int b) {
-			System.out.println(a + "÷" + b + "は？");
-			if(b == 0) {
-				throw new IllegalArgumentException("引数が不正です");
-			}
-			int result = a / b;
-			System.out.println("計算結果" + result);
-		}
+//		public static void division(int a, int b) {
+//			System.out.println(a + "÷" + b + "は？");
+//			if(b == 0) {
+//				throw new IllegalArgumentException("引数が不正です");
+//			}
+//			int result = a / b;
+//			System.out.println("計算結果" + result);
+//		}
 	}
 
 }
---
+++ JavaWcp/src/chapter10/Exception04.java
@@ -0,0 +1,20 @@
+package chapter10;
+
+public class Exception04 {
+	public static void main(String[] args) {
+//		try {
+//			Exception04.readFile("exception.txt");
+//		} catch (FileNotFoundException e) {
+//			System.out.println("FileNptFoundException例外が発生");
+//		}
+//		System.out.println("プログラム終了");
+//
+//		public static void readFile(String fileName) throws FileNotFoundException {
+//			System.out.println("ファイルの読み込み開始");
+//			FileReader fr = new FileReader(fileName);
+//			System.out.println(fileName + "の読み込み完了");
+//		}
+
+	}
+
+}
